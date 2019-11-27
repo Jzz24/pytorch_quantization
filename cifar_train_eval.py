dc2eff5e66ca9a6b7f876a66cfd43adeb6069f64
纪喆 <jizhe@jizhedeMacBook-Pro.local>
2019-11-27 21:16:24 +0800

    first commit

---
+++ cifar_train_eval.py
@@ -0,0 +1,177 @@
+import os
+import time
+import numpy
+import argparse
+from datetime import datetime
+
+import torch
+import torch.optim as optim
+import torch.backends.cudnn as cudnn
+from torch.utils.tensorboard import SummaryWriter
+
+
+cudnn.benchmark = True
+import torchvision
+
+
+from models.resnet import *
+
+
+from utils.preprocess import *
+from utils.bar_show import progress_bar
+
+# Training settings
+parser = argparse.ArgumentParser(description='dorefa-net implementation')
+
+parser.add_argument('--root_dir', type=str, default='./')
+parser.add_argument('--data_dir', type=str, default='./data')
+parser.add_argument('--log_name', type=str, default='resnet_4w4a')
+parser.add_argument('--pretrain', action='store_true', default=False)
+parser.add_argument('--pretrain_dir', type=str, default='./ckpt/resnet_2w2a/')
+
+
+parser.add_argument('--cifar', type=int, default=10)
+parser.add_argument('--lr', type=float, default=0.1)
+parser.add_argument('--wd', type=float, default=1e-4)
+parser.add_argument('--train_batch_size', type=int, default=256)
+parser.add_argument('--eval_batch_size', type=int, default=100)
+parser.add_argument('--max_epochs', type=int, default=200)
+parser.add_argument('--log_interval', type=int, default=40)
+parser.add_argument('--num_workers', type=int, default=2)
+
+cfg = parser.parse_args()
+
+best_acc = 0  # best test accuracy
+start_epoch = 0
+
+cfg.log_dir = os.path.join(cfg.root_dir, 'logs', cfg.log_name)
+cfg.ckpt_dir = os.path.join(cfg.root_dir, 'ckpt', cfg.log_name)
+
+os.makedirs(cfg.log_dir, exist_ok=True)
+os.makedirs(cfg.ckpt_dir, exist_ok=True)
+
+def main():
+    if cfg.cifar == 10:
+        print('training CIFAR-10 !')
+        dataset = torchvision.datasets.CIFAR10
+    elif cfg.cifar == 100:
+        print('training CIFAR-100 !')
+        dataset = torchvision.datasets.CIFAR100
+    else:
+        assert False, 'dataset unknown !'
+
+    print('===> Preparing data ..')
+    train_dataset = dataset(root=cfg.data_dir, train=True, download=True,
+                          transform=cifar_transform(is_training=True))
+    train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=cfg.train_batch_size, shuffle=True,
+                                             num_workers=cfg.num_workers)
+
+    eval_dataset = dataset(root=cfg.data_dir, train=False, download=True,
+                         transform=cifar_transform(is_training=False))
+    eval_loader = torch.utils.data.DataLoader(eval_dataset, batch_size=cfg.eval_batch_size, shuffle=False,
+                                            num_workers=cfg.num_workers)
+
+    print('===> Building ResNet..')
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    model = ResNet18().to(device)
+
+    if device == 'cuda':
+        model = torch.nn.DataParallel(model)
+        cudnn.benchmark = True
+
+
+    optimizer = torch.optim.SGD(model.parameters(), lr=cfg.lr, momentum=0.9, weight_decay=cfg.wd)
+    lr_schedu = optim.lr_scheduler.MultiStepLR(optimizer, [100, 150, 180], gamma=0.1)
+    criterion = torch.nn.CrossEntropyLoss().cuda()
+    summary_writer = SummaryWriter(cfg.log_dir)
+
+    if cfg.pretrain:
+        ckpt = torch.load(os.path.join(cfg.ckpt_dir, f'checkpoint.t7'))
+        model.load_state_dict(ckpt['model_state_dict'])
+        optimizer.load_state_dict(ckpt['optimizer_state_dict'])
+        start_epoch = ckpt['epoch']
+        print('===> Load last checkpoint data')
+    else:
+        start_epoch = 0
+        print('===> Start from scratch')
+
+
+    def train(epoch):
+        print('\nEpoch: %d' % epoch)
+        model.train()
+        train_loss, correct, total = 0, 0 ,0
+
+        for batch_idx, (inputs, targets) in enumerate(train_loader):
+            inputs, targets = inputs.to('cuda'), targets.to('cuda')
+            optimizer.zero_grad()
+            outputs = model(inputs)
+            loss = criterion(outputs, targets)
+            loss.backward()
+            optimizer.step()
+
+            train_loss += loss.item()
+            _, predicted = outputs.max(1)
+            total += targets.size(0)
+            correct += predicted.eq(targets).sum().item()
+
+            progress_bar(batch_idx, len(train_loader), 'Loss: %.3f | Acc: %.3f%% (%d/%d)'
+                         % (train_loss / (batch_idx + 1), 100. * correct / total, correct, total))
+
+            if batch_idx % cfg.log_interval == 0:  #every log_interval mini_batches...
+                summary_writer.add_scalar('Loss/train', train_loss / (batch_idx + 1), epoch * len(train_loader) + batch_idx)
+                summary_writer.add_scalar('Accuracy/train', 100. * correct / total, epoch * len(train_loader) + batch_idx)
+                summary_writer.add_scalar('learning rate', optimizer.param_groups[0]['lr'], epoch * len(train_loader) + batch_idx)
+                # for tag, value in model.named_parameters():
+                #     tag = tag.replace('.', '/')
+                #     summary_writer.add_histogram(tag, value.detach(), global_step=epoch * len(train_loader) + batch_idx)
+                #     summary_writer.add_histogram(tag + '/grad', value.grad.detach(), global_step=epoch * len(train_loader) + batch_idx)
+
+
+
+
+    def test(epoch):
+        # pass
+        global best_acc
+        model.eval()
+
+        test_loss, correct, total = 0, 0, 0
+        with torch.no_grad():
+            for batch_idx, (inputs, targets) in enumerate(eval_loader):
+                inputs, targets = inputs.to('cuda'), targets.to('cuda')
+                outputs = model(inputs)
+                loss = criterion(outputs, targets)
+
+                test_loss += loss.item()
+                _, predicted = outputs.max(1)
+                total += targets.size(0)
+                correct += predicted.eq(targets).sum().item()
+
+                progress_bar(batch_idx, len(eval_loader), 'Loss: %.3f | Acc: %.3f%% (%d/%d)'
+                    % (test_loss/(batch_idx+1), 100.*correct/total, correct, total))
+
+                if batch_idx % cfg.log_interval == 0:  # every log_interval mini_batches...
+                    summary_writer.add_scalar('Loss/test', test_loss / (batch_idx + 1), epoch * len(train_loader) + batch_idx)
+                    summary_writer.add_scalar('Accuracy/test', 100. * correct / total, epoch * len(train_loader) + batch_idx)
+
+        acc = 100. * correct / total
+        if acc > best_acc:
+            print('Saving..')
+            state = {
+                'model_state_dict': model.state_dict(),
+                'optimizer_state_dict': optimizer.state_dict(),
+                'acc': acc,
+                'epoch': epoch,
+            }
+            torch.save(state, os.path.join(cfg.ckpt_dir, f'checkpoint.t7'))
+            best_acc = acc
+
+    for epoch in range(start_epoch, cfg.max_epochs):
+        train(epoch)
+        test(epoch)
+        lr_schedu.step(epoch)
+    summary_writer.close()
+
+
+if __name__ == '__main__':
+    main()
+
---
+++ models/resnet.py
@@ -0,0 +1,146 @@
+import torch
+import torch.nn as nn
+# from utils.quant_dorefa import *
+from .quant_dorefa import QuanConv as Conv
+from .quant_dorefa import Linear_Q
+
+
+import torch.nn.functional as F
+
+
+def conv3x3(in_planes, out_planes, stride=1):
+    """3x3 convolution with padding"""
+    return Conv(in_planes, out_planes, 3, stride=stride, padding=1, bias=False)
+
+
+def conv1x1(in_planes, out_planes, stride=1):
+    """1x1 convolution"""
+    return Conv(in_planes, out_planes, kernel_size=1, stride=stride, bias=False)
+
+def linear(in_featrues, out_features):
+    return Linear_Q(in_featrues, out_features)
+
+
+class BasicBlock(nn.Module):
+    expansion = 1
+
+    def __init__(self, in_planes, planes, stride=1):
+        super(BasicBlock, self).__init__()
+        self.conv1 = conv3x3(in_planes, planes, stride=stride)
+        self.bn1 = nn.BatchNorm2d(planes)
+        self.conv2 = conv3x3(planes, planes, stride=1)
+        self.bn2 = nn.BatchNorm2d(planes)
+
+        self.shortcut = nn.Sequential()
+        if stride != 1 or in_planes != self.expansion*planes:
+            self.shortcut = nn.Sequential(
+                conv1x1(in_planes, self.expansion*planes, stride=stride),
+                nn.BatchNorm2d(self.expansion*planes)
+            )
+
+    def forward(self, x):
+        out = F.relu(self.bn1(self.conv1(x)))
+        out = self.bn2(self.conv2(out))
+        out += self.shortcut(x)
+        out = F.relu(out)
+        return out
+
+
+class Bottleneck(nn.Module):
+    expansion = 4
+
+    def __init__(self, in_planes, planes, stride=1):
+        super(Bottleneck, self).__init__()
+        self.conv1 = conv3x3(in_planes, planes, kernel_size=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(planes)
+        self.conv2 = conv3x3(planes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(planes)
+        self.conv3 = conv3x3(planes, self.expansion*planes, kernel_size=1, bias=False)
+        self.bn3 = nn.BatchNorm2d(self.expansion*planes)
+
+        self.shortcut = nn.Sequential()
+        if stride != 1 or in_planes != self.expansion*planes:
+            self.shortcut = nn.Sequential(
+                conv1x1(in_planes, self.expansion*planes, kernel_size=1, stride=stride, bias=False),
+                nn.BatchNorm2d(self.expansion*planes)
+            )
+
+    def forward(self, x):
+        out = F.relu(self.bn1(self.conv1(x)))
+        out = F.relu(self.bn2(self.conv2(out)))
+        out = self.bn3(self.conv3(out))
+        out += self.shortcut(x)
+        out = F.relu(out)
+        return out
+
+class ResNet(nn.Module):
+    def __init__(self, block, num_blocks, num_classes=10):
+        super(ResNet, self).__init__()
+        self.in_planes = 64
+
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1, bias=False)  # do not quntize the first layer
+        # self.conv1 = Conv(3, 64, kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(64)
+        self.layer1 = self._make_layer(block, 64, num_blocks[0], stride=1)
+        self.layer2 = self._make_layer(block, 128, num_blocks[1], stride=2)
+        self.layer3 = self._make_layer(block, 256, num_blocks[2], stride=2)
+        self.layer4 = self._make_layer(block, 512, num_blocks[3], stride=2)
+        self.linear = linear(512*block.expansion, num_classes)
+        # self.linear = nn.Linear(512 * block.expansion, num_classes)
+
+        # weight initialization
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight, mode='fan_out')
+                if m.bias is not None:
+                    nn.init.zeros_(m.bias)
+            elif isinstance(m, nn.BatchNorm2d):
+                nn.init.ones_(m.weight)
+                nn.init.zeros_(m.bias)
+            elif isinstance(m, nn.Linear):
+                nn.init.normal_(m.weight, 0, 0.01)
+                nn.init.zeros_(m.bias)
+
+
+    def _make_layer(self, block, planes, num_blocks, stride):
+        strides = [stride] + [1]*(num_blocks-1)
+        layers = []
+        for stride in strides:
+            layers.append(block(self.in_planes, planes, stride))
+            self.in_planes = planes * block.expansion
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+
+        out = F.relu(self.bn1(self.conv1(x)))
+        out = self.layer1(out)
+        out = self.layer2(out)
+        out = self.layer3(out)
+        out = self.layer4(out)
+        out = F.avg_pool2d(out, 4)
+        out = out.view(out.size(0), -1)
+        out = self.linear(out)
+        return out
+
+def ResNet18():
+    return ResNet(BasicBlock, [2,2,2,2])
+
+def ResNet34():
+    return ResNet(BasicBlock, [3,4,6,3])
+
+def ResNet50():
+    return ResNet(Bottleneck, [3,4,6,3])
+
+def ResNet101():
+    return ResNet(Bottleneck, [3,4,23,3])
+
+def ResNet152():
+    return ResNet(Bottleneck, [3,8,36,3])
+
+
+def test():
+    net = ResNet18()
+    print (net)
+    y = net(torch.ones(1,3,32,32))
+    print(y)
+
